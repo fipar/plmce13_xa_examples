70eaf09758298123c4db39562c0f8537e75c8c02
Fernando Ipar <fipar@acm.org>
2013-01-06 21:20:40 -0200

    modified the base example to use the Configuration module

--- xa.rb
+++ xa.rb
@@ -2,24 +2,26 @@
 # first basic example based on http://blog.endpoint.com/2010/07/distributed-transactions-and-two-phase.html
 
 require 'java'
+require 'config'
 
 BTM = Java::BitronixTm::bitronixTransactionManager
 TxnSvc = Java::BitronixTm::TransactionManagerServices
 PDS = Java::BitronixTmResourceJdbc::PoolingDataSource
 
 ds1 = PDS.new
-ds1.set_class_name 'com.mysql.jdbc.jdbc2.optional.MysqlXADataSource'
+ds1.set_class_name Configuration::DB_CONFIG_1["class_name"]
 ds1.set_unique_name 'mysql1'
 ds1.set_max_pool_size 3
-ds1.get_driver_properties.set_property 'url', 'jdbc:mysql://127.0.0.1:5527/test?user=msandbox&password=msandbox'
+ds1.get_driver_properties.set_property 'url', Configuration::DB_CONFIG_1["url"]
 ds1.init
 
 ds2 = PDS.new
-ds2.set_class_name 'com.mysql.jdbc.jdbc2.optional.MysqlXADataSource'
+ds2.set_class_name Configuration::DB_CONFIG_2["class_name"]
 ds2.set_unique_name 'mysql2'
 ds2.set_max_pool_size 3
-ds2.get_driver_properties.set_property 'url', 'jdbc:mysql://127.0.0.1:5512/test?user=msandbox&password=msandbox'
+ds2.get_driver_properties.set_property 'url', Configuration::DB_CONFIG_2["url"]
 ds2.init
+
 
 c1 = ds1.get_connection
 c2 = ds2.get_connection
